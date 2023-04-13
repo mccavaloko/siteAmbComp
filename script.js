7928f481a27571b73c5489e3095637e5ab849cb1
Miguel Sena <miguelaugustosena@gmail.com>
2023-04-13 11:14:03 -0300

    Jquery para modal sobre nós

--- script.js
+++ script.js
@@ -10,3 +10,14 @@ const toggleBtn = document.querySelector('.toggle_btn');
             ? 'fa-solid fa-xmark'
             : 'fa-solid fa-bars';
     }
+$(".modal-trigger").click(function(e){
+e.preventDefault();
+dataModal = $(this).attr("data-modal");
+$("#" + dataModal).css({"display":"block"});
+// $("body").css({"overflow-y": "hidden"}); //Prevent double scrollbar.
+});
+
+$(".close-modal, .modal-sandbox").click(function(){
+$(".modal").css({"display":"none"});
+// $("body").css({"overflow-y": "auto"}); //Prevent double scrollbar.
+});
